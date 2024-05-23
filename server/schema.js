033dbb0dc6bd50caa64134db5b3d8bda1172786b
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-05-23 22:22:11 +0530

    const TicketSchema = new mongoose.Schema({ ... });: This section defines a Mongoose schema named TicketSchema. A schema in Mongoose defines the structure of documents (records) within a collection (table) in a MongoDB database. In this case, the schema describes the structure of a ticket for booking a movie. It includes fields for the movie name (movie), time slot (slot), and a nested object seats which contains specific seat numbers (A1, A2, etc.) and the corresponding number of seats booked for each.

--- server/schema.js
+++ server/schema.js
@@ -1,2 +1,14 @@
 const mongoose = require('mongoose');
-
+// Creating a new schema for storing movie booking details.
+const TicketSchema = new mongoose.Schema({
+  movie: { type: String },
+  slot: { type: String },
+  seats: {
+    A1: { type: Number },
+    A2: { type: Number },
+    A3: { type: Number },
+    A4: { type: Number },
+    D1: { type: Number },
+    D2: { type: Number },
+  },
+});
