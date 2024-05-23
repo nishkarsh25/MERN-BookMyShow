fa623c6aedae0493c2b701a8d4bf156fa5ca95d7
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-05-23 22:20:03 +0530

    console.log('Connected to Database'): If the connection to the database is successful, this line logs a message to the console indicating that the connection was successful.

--- server/dbConnection.js
+++ server/dbConnection.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     await mongoose.connect('mongodb://127.0.0.1:27017/bookMyShow');
-    
+    console.log('Connected to Database')
   }
   catch (error) {
     
