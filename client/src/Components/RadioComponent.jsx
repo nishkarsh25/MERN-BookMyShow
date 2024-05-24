eb6c528cd774c751aea2160a6e0294a938440b49
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-05-24 06:41:25 +0530

    Event Handler Function handleChecked Function: This is an internal function within the RadioComponent that takes a value as an argument. When called, it invokes the changeSelection function passed via props with this value. Essentially, it updates the selected option.

--- client/src/Components/RadioComponent.jsx
+++ client/src/Components/RadioComponent.jsx
@@ -3,7 +3,9 @@ import React from "react";
 // This is the radio component which is used for selecting movie and slot.
 const RadioComponent = ({ text, changeSelection, data }) => {
   // Changing selected data from the user.
-  
+  const handleChecked = (value) => {
+    changeSelection(value);
+  };
 
   
 };
