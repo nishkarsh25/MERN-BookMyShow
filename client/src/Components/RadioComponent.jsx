caffef8280a2a5825c072beb4dcac02cfec754cf
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-05-24 06:41:15 +0530

    Props Explanation Props: text: This prop represents the label or text for the radio button. It is the option that the user can select. changeSelection: This prop is a function that will be called when the user selects this radio option. It updates the selected value in the parent component or context. data: This prop holds the current selected value. It is used to determine if this radio option is currently selected.

--- client/src/Components/RadioComponent.jsx
+++ client/src/Components/RadioComponent.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 // This is the radio component which is used for selecting movie and slot.
 const RadioComponent = ({ text, changeSelection, data }) => {
+  // Changing selected data from the user.
   
 
   
