3f4f24cf25000f238285506a2d7b5e2bede49c30
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-05-24 07:00:59 +0530

    Exporting slots:
    The slots array is exported using exports. This makes the slots array available for import in other modules. By exporting it, other parts of the application can access and use the list of time slots.

--- client/src/data.jsx
+++ client/src/data.jsx
@@ -2,4 +2,4 @@ let moviesList = ['Justice League', 'Tenet', 'Fast X', 'Planet of the Apes', 'Co
 let slots = ['10:00 AM', '01:00 PM', '03:00 PM', "08:00 PM"];
 let seats = ['A1', 'A2', 'A3', 'A4', 'D1', 'D2'];
 exports.moviesList = moviesList;
-
+exports.slots = slots;
