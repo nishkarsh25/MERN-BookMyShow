b09122c88f70dbe2e7c04ea6749821d8c7ba1df8
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-05-24 06:16:56 +0530

    Defining the POST Route:
    A POST route is defined at the /booking endpoint. This route is responsible for handling requests to create a new movie booking.
    Extracting Data from the Request Body:
    
    The route handler extracts the movie, slot, and seats fields from the request body. These fields represent the details of the movie booking that the client wants to create.

--- server/routes.js
+++ server/routes.js
@@ -4,7 +4,7 @@ const Ticket = require("./schema"); // Import the Ticket schema
 
 // Endpoint for creating a new booking and adding it to the database.
 router.post("/booking", async (req, res) => {
-  
+  const { movie, slot, seats } = req.body;
 
   
 });
