3dbe47eb86935f21397effa140686eadb0b4c2f8
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-05-24 07:26:54 +0530

    Home Component: Detailed Explanation The Home component is the main page of a movie booking application, bringing together several components and managing the overall booking logic. Here’s a detailed breakdown of each part of the component:
    Imports
    Component Imports: Several components are imported from the Components directory, including LastBookingDetails, SelectMovie, SelectSeats, TimeShedule, and Modal.
    Context Import: The BsContext is imported to provide and manage the state across the components.
    useContext Hook: The useContext hook is imported from React to use the context values within this component.

---
+++ client/src/Pages/Home.jsx
@@ -0,0 +1,11 @@
+import LastBookingDetails from "../Components/LastBookingDetails";
+import SelectMovie from "../Components/SelectMovie";
+import SelectSeats from "../Components/SelectSeats";
+import TimeShedule from "../Components/TimeShedule";
+import Modal from "../Components/ModalComponent";
+import BsContext from "../Context/BsContext";
+import { useContext } from "react";
+
+
+
+
