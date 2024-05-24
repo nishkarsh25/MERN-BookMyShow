31ad79b9f5b0fd4dcb85480174e38b79199abb26
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-05-24 06:29:55 +0530

    State Variables:
    Various state variables are initialized using useState:
    errorPopup and errorMessage to manage and display error messages.
    time, movie, and noOfSeat to store the booking details like movie name, time slot, and number of seats for different seat types.
    lastBookingDetails to store the details of the last booking made.
    seat to manage the selection of seats (added to handle seat selections specifically).

--- client/src/Context/BsState.jsx
+++ client/src/Context/BsState.jsx
@@ -3,7 +3,21 @@ import BsContext from "./BsContext";
 import axios from 'axios';
 
 const BsState = (props) => {
-  
+  const [errorPopup, setErrorPopup] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [time, changeTime] = useState("");
+  const [movie, changeMovie] = useState("");
+  const [noOfSeat, changeNoOfSeats] = useState({
+    A1: "",
+    A2: "",
+    A3: "",
+    A4: "",
+    D1: "",
+    D2: "",
+  });
+  const [lastBookingDetails, setLastBookingDetails] = useState(null);
+
+  const [seat, changeSeats] = useState([]); // Added this line
 
   
 
