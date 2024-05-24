5d6c6f5279ad3ffe5462645e446365272b166c12
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-05-24 07:19:23 +0530

    Booking Details Seats Information:
    When lastBookingDetails is available, a nested div element displays the seats information. This includes:
    A p element for the seats label.
    An unordered list (ul) containing list items (li) for each seat. Each list item is styled with background color, text color, padding, and margin classes.
    The seat identifier and the corresponding number of seats booked are displayed in each list item.
    Slot and Movie Information:
    
    Below the seats information, p elements display the slot and movie details of the last booking. These elements are styled with text size, weight, and color classes.

--- client/src/Components/LastBookingDetails.jsx
+++ client/src/Components/LastBookingDetails.jsx
@@ -15,7 +15,20 @@ const LastBookingDetails = () => {
     <div className="bg-gradient-to-r from-purple-500 via-pink-500 to-red-500 p-8 rounded-lg shadow-lg">
       <h2 className="text-2xl font-semibold text-white mb-6">Last Booking Details:</h2>
       {lastBookingDetails ? (
-        
+        <>
+          <div className="mb-6">
+            <p className="text-lg font-semibold mb-2 text-white">Seats:</p>
+            <ul className="flex flex-wrap">
+              {seats.map((seat, index) => (
+                <li className="bg-blue-500 text-white rounded-md py-2 px-4 mr-4 mb-4" key={index}>
+                  {seat}: <span className="font-semibold">{Number(lastBookingDetails.seats[seat])}</span>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <p className="text-lg font-semibold mb-4 text-white">Slot: <span className="font-normal">{lastBookingDetails.slot}</span></p>
+          <p className="text-lg font-semibold text-white">Movie: <span className="font-normal">{lastBookingDetails.movie}</span></p>
+        </>
       ) : (
         
       )}
