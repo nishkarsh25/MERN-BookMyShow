fe6ffde7439e054e791deb8d1f27842ebe7b3f0d
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-05-24 07:22:47 +0530

    Context Destructuring Destructuring Context Values: Within the component, the context values are destructured to extract errorPopup, errorMessage, setErrorPopup, and setErrorMessage. errorPopup is a boolean indicating whether the error popup should be displayed. errorMessage contains the message to be displayed in the popup. setErrorPopup is a function to update the visibility of the error popup. setErrorMessage is a function to update the error message.

--- client/src/Components/ModalComponent.jsx
+++ client/src/Components/ModalComponent.jsx
@@ -2,8 +2,8 @@ import React, { useContext } from "react";
 import BsContext from "../Context/BsContext";
 
 function Modal(props) {
-  
-  
+  const context = useContext(BsContext);
+  const { errorPopup, errorMessage, setErrorPopup, setErrorMessage } = context;
 
   
 
