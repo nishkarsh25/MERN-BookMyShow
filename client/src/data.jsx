95d4b6e7b0c4cfe35ab75a28a07ef3caa43cfc08
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-05-24 07:00:49 +0530

    Exporting Arrays Exporting moviesList:
    The moviesList array is exported using exports. This makes the moviesList array available for import in other modules. By exporting it, other parts of the application can access and use the list of movie titles.

--- client/src/data.jsx
+++ client/src/data.jsx
@@ -1,5 +1,5 @@
 let moviesList = ['Justice League', 'Tenet', 'Fast X', 'Planet of the Apes', 'Come Play'];
 let slots = ['10:00 AM', '01:00 PM', '03:00 PM', "08:00 PM"];
 let seats = ['A1', 'A2', 'A3', 'A4', 'D1', 'D2'];
-
+exports.moviesList = moviesList;
 
