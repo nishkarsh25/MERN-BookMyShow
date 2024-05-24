ba27b5decb092a3610aa049199d42fc0ac0e8f2b
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-05-24 06:40:29 +0530

    Defining the RadioComponent RadioComponent Definition: The RadioComponent is defined as a functional component. It takes in three props: text, changeSelection, and data.

--- client/src/Components/RadioComponent.jsx
+++ client/src/Components/RadioComponent.jsx
@@ -1,4 +1,10 @@
 import React from "react";
 
+// This is the radio component which is used for selecting movie and slot.
+const RadioComponent = ({ text, changeSelection, data }) => {
+  
+
+  
+};
 
 
