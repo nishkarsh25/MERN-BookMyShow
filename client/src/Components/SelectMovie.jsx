1f36294d6c95f9141778a99b7f7805239171a863
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-05-24 06:37:44 +0530

    Defining the SelectMovie Component SelectMovie Component: The SelectMovie component is defined as a functional component. This component will render the UI for selecting a movie.

--- client/src/Components/SelectMovie.jsx
+++ client/src/Components/SelectMovie.jsx
@@ -3,5 +3,14 @@ import RadioComponent from "./RadioComponent";
 import { moviesList } from "../data";
 import BsContext from "../Context/BsContext";
 
+const SelectMovie = () => {
+  
+
+  
+
+  
+
+  
+};
 
 
