7f1cf198ac04bc1c5ea661a85361a865da4ae894
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-05-24 07:15:46 +0530

    Component Definition LastBookingDetails Component: The LastBookingDetails component is defined as a functional component. It uses the useContext hook to access the context values and functions provided by BsContext.

--- client/src/Components/LastBookingDetails.jsx
+++ client/src/Components/LastBookingDetails.jsx
@@ -2,6 +2,13 @@ import React, { useEffect, useContext } from "react";
 import BsContext from "../Context/BsContext";
 import { seats } from "../data";
 
+const LastBookingDetails = () => {
+  
+  
 
+  
+
+  
+};
 
 
