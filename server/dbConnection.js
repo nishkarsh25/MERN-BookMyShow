d36341f34506cf16fd63f1ec55c11abb6844d878
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-08-21 17:22:14 +0530

    Forgot to change my mongodb atlas link:

--- server/dbConnection.js
+++ server/dbConnection.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://127.0.0.1:27017/bookMyShow');
-    console.log('Connected to Database')
-  }
-  catch (error) {
-    console.log(error)
+    await mongoose.connect(process.env.MONGODB_ATLAS_URL);
+    console.log('Connected to Database');
+  } catch (error) {
+    console.log(error);
   }
 }
+
 module.exports = connectDB;
